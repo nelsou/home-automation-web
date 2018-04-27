edfc6521446e224258912d3023f55a5ee050d928
Nelson <nelsou@github.com>
2018-04-27 20:00:48 +0200

    * Run exec

--- src/main/java/com/home/automation/ws/Application.java
+++ src/main/java/com/home/automation/ws/Application.java
@@ -2,11 +2,12 @@ package com.home.automation.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 @EnableJpaRepositories
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
--- src/main/java/com/home/automation/ws/HomeController.java
+++ src/main/java/com/home/automation/ws/HomeController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @RestController
 public class HomeController {
 
@@ -16,12 +21,27 @@ public class HomeController {
     private PlayService playService;
 
     @RequestMapping("/")
-    public String index() {
+    public String index() throws IOException, InterruptedException {
+        HomeController.run("ls");
         return "Made with <3";
     }
 
     @RequestMapping("/play/{name}")
     public ResultResponse play(@PathVariable String name, @RequestParam String cardName) {
         return playService.play(name, CardEnum.valueOf(cardName));
+    }
+
+    public static void run(String... args) throws java.io.IOException, java.lang.InterruptedException {
+        java.lang.Runtime rt = java.lang.Runtime.getRuntime();
+        java.lang.Process p = rt.exec(args[0]);
+        p.waitFor();
+
+        try (InputStream is = p.getInputStream();
+             BufferedReader reader = new java.io.BufferedReader(new InputStreamReader(is))) {
+            String s = null;
+            while ((s = reader.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
     }
 }
